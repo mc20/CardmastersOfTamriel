3df7e15c8ea31185cdb133f03b14110f6565e66d
Michael <2105853+mc20@users.noreply.github.com>
2024-11-13 12:29:30 -0800

    Update JSON configuration files for NPCs and distribution; refactor MiscItemFactory to improve default value handling

--- CardmastersOfTamriel.SynthesisPatcher/MiscItems/Factory/MiscItemFactory.cs
+++ CardmastersOfTamriel.SynthesisPatcher/MiscItems/Factory/MiscItemFactory.cs
@@ -1,4 +1,5 @@
-﻿using CardmastersOfTamriel.SynthesisPatcher.Utilities;
+﻿using CardmastersOfTamriel.Models;
+using CardmastersOfTamriel.SynthesisPatcher.Utilities;
 using Mutagen.Bethesda.Skyrim;
 using Serilog;
 
@@ -11,11 +12,20 @@ public static class MiscItemFactory
         var editorId = $"MiscItem_SERIES_{card.SeriesId}_CARD_{card.Id}".AddModNamePrefix();
         var newMiscItem = skyrimMod.MiscItems.AddNewWithId(editorId);
         newMiscItem.Name = card.DisplayName;
-        newMiscItem.Value = card.Value == 0 ? 10 : card.Value;
+        newMiscItem.Value = card.Value == 0 ? GetDefaultValue(card) : card.Value;
         newMiscItem.Weight = card.Weight;
 
         Log.Verbose($"Added MiscItem {newMiscItem.EditorID} with Name: '{newMiscItem.Name}'");
 
         return newMiscItem;
     }
+
+    private static uint GetDefaultValue(Card card) => card.Tier switch
+    {
+        CardTier.Tier1 => 10,
+        CardTier.Tier2 => 75,
+        CardTier.Tier3 => 150,
+        CardTier.Tier4 => 250,
+        _ => 1000
+    };
 }
